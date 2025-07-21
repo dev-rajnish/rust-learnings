9ce60d0f7b17da02e7d051c782a5b199990ec67d
dev-rajnish <dev.rajnish@proton.me>
2025-07-22 01:04:36 +0530

    init

--- char_dance/target/debug/char_dance.d
+++
@@ -1,1 +0,0 @@
-/home/rsh/rusty/char_dance/target/debug/char_dance: /home/rsh/rusty/char_dance/src/main.rs
--- char_dance/target/debug/deps/char_dance-00b3fba7d4adcc47.d
+++
@@ -1,5 +0,0 @@
-/home/rsh/rusty/char_dance/target/debug/deps/char_dance-00b3fba7d4adcc47.d: src/main.rs
-
-/home/rsh/rusty/char_dance/target/debug/deps/char_dance-00b3fba7d4adcc47: src/main.rs
-
-src/main.rs:
