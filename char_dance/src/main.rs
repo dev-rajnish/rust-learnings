fd53c6ccdf8fd8259580f70c049abe0e8e45d497
dev-rajnish <dev.rajnish@proton.me>
2025-07-22 02:56:30 +0530

    rust learnings init

--- char_dance/src/main.rs
+++ char_dance/src/main.rs
@@ -1,31 +1,63 @@
 use std::{thread, time::Duration};
 
 fn print_delay() {
-    thread::sleep(Duration::from_millis(30));
+    thread::sleep(Duration::from_millis(40));
+}
+
+fn print_border(distance_at: u8) {
+    print!("\x1B[{}G", distance_at);
+    print!("||");
 }
 
 fn forward_char() {
     for i in 60..70 {
         // Convert the integer to a character
-        if let Some(c) = char::from_u32(i as u32) {
-            //print newline and space
-            print!("\n\x1B[{i}G");
+        if let Some(c) = char::from_u32(i as u32 + '.' as u32) {
+            print_border(40);
+            //print space
+            print!("\x1B[{}G", i);
+
             //print char colored
             print!("\x1B[{}m {c} \x1B[0m", i + 30);
+            // print!("\x1B[{}m {c} \x1B[0m", i + 30);
+
             print_delay();
+
+            //printing in rev
+            print!("\x1B[{}G", 130 - i);
+            print!("\x1B[{}m {c} \x1B[0m", i + 30);
+            // print!("\x1B[{}m {c} \x1B[0m", i + 30);
+
+            print_delay();
+            println!();
+
+            print_border(90);
         }
     }
 }
 
 fn backward_char() {
     for i in (62..70).rev() {
-        // Convert the integer to a character
         if let Some(c) = char::from_u32(i as u32) {
-            //print newline and space
-            print!("\n\x1B[{i}G");
+            print_border(40);
+
+            //print space
+            print!("\x1B[{}G", i);
+
             //print char colored
             print!("\x1B[{}m {c} \x1B[0m", i - 40);
+            // print!("\x1B[{}m {c} \x1B[0m", i - 40);
+
             print_delay();
+            //printing in rev
+            print!("\x1B[{}G", 130 - i + 1);
+            print!("\x1B[{}m {c} \x1B[0m", i - 40);
+            //print!("\x1B[{}m {c} \x1B[0m", i - 40);
+
+            print_delay();
+            println!();
+
+            print_border(90);
         }
     }
 }
